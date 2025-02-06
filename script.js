3e92859813dc240152d2f135bdad4ab446d3f84e
Albert <153892855+Albrt78@users.noreply.github.com>
2025-02-06 17:39:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const hamburgerMenu = document.querySelector("#panel");
+const navbar = document.querySelector("#panel");
 const hamburgerButton = document.querySelector("#hamburger-icon");
 
 // Function for hamburger Button
 const changeIcon = (icon) => {
-    hamburgerMenu.classList.toggle("active");
+    navbar.classList.toggle("active");
     icon.classList.toggle("fa-square-xmark");
 };
